458e6c1d68a3340645965df204b24d9fe6d13763
Muhammad Audi Agung Pratama <audi.pratama@traveloka.com>
2024-04-06 14:38:15 +0700

    env

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import movieReducer from "../features/movieSlice";
 import logger from "redux-logger";
 
+const shouldIncludeLogger = process.env.NODE_ENV === "development"
+
 export const store = configureStore({
   reducer: {
     movie: movieReducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+  middleware: (getDefaultMiddleware) =>
+    shouldIncludeLogger ? getDefaultMiddleware().concat(logger) : getDefaultMiddleware()
 })
